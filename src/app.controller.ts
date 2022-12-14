ea3af0a251ff166e49f006304ae754515adf1e2e
PavitraIR <pavitrairanna.rampure@tibilsolutions.com>
2022-12-14 11:39:04 +0530

    schema changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {schema} from './ingestion-data';
 
 @Controller()
 export class AppController {
---
+++ src/ingestion-data.ts
@@ -0,0 +1,7 @@
+const schema= [{"ingestionType": "dimension",
+    "dimenstion_name": "string",
+    "name": "string",
+    "district_id": "string"
+
+}]
+export {schema};
