7bbea960f3503d65825285eb2704537179ef3541
Theja G D <theja@appzui.com>
2022-12-14 13:54:30 +0530

    Removed nest files

--- src/ingestion-data.ts
+++
@@ -1,7 +0,0 @@
-const schema= [{"ingestionType": "dimension",
-    "dimenstion_name": "string",
-    "name": "string",
-    "district_id": "string"
-
-}]
-export {schema};
--- src/ingestion/ingestion.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IngestionController } from './controller/ingestion.controller';
-import { IngestionService } from './service/ingestion.service';
-
-@Module({
-  controllers: [IngestionController],
-  providers: [IngestionService]
-})
-export class IngestionModule {}
