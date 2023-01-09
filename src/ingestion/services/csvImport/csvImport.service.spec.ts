19b0a6b1f32c8f5ce28f29f1ec53e92efd03a22a
Theja G D <theja@appzui.com>
2023-01-09 18:58:35 +0530

    Code coverage completed

--- src/ingestion/services/csvImport/csvImport.service.spec.ts
+++ src/ingestion/services/csvImport/csvImport.service.spec.ts
@@ -3,18 +3,9 @@ import {GenericFunction} from '../generic-function';
 import {CsvImportService} from './csvImport.service';
 import {HttpCustomService} from "../HttpCustomService";
 import * as fs from 'fs';
-import {promisify} from 'util';
 
 
-const mockFile = {
-    fieldname: 'file',
-    originalname: 'list.csv',
-    encoding: '7bit',
-    mimetype: 'text/csv',
-    path: 'list.csv',
-    buffer: Buffer.from('list.csv', 'utf8'),
-    size: 51828,
-} as Express.Multer.File;
+const mockFilePath = './files/list.csv';
 
 describe('csvImportService', () => {
     let service: CsvImportService;
@@ -70,47 +61,125 @@ describe('csvImportService', () => {
             ]
         };
         try {
-            await service.readAndParseFile(inputData, mockFile);
+            await service.readAndParseFile(inputData, mockFilePath);
         } catch (e) {
             expect(e).toEqual(resultOutput);
         }
     });
 
-    it('Valid Input', async () => {
-        await createFile('./files/', 'list.csv', "school_id,grade,count\n1,2,3\n4,5,6");
-
-        const mockFile = {
-            fieldname: 'file',
-            originalname: 'list.csv',
-            encoding: '7bit',
-            mimetype: 'text/csv',
-            path: './files/list.csv',
-            buffer: Buffer.from('list.csv', 'utf8'),
-            size: 51828,
-        } as Express.Multer.File;
+    it('Valid large Input', async () => {
+        const filePath = createNumberOfLineCSVFile(['school_id','grade','count'],1003,'list_valid_large.csv');
 
         const inputData = {
             "ingestion_type": "event",
             "ingestion_name": "student_attendance"
         };
         let resultOutput = {code: 200, message: 'CSV Uploaded Successfully'};
-        expect(await service.readAndParseFile(inputData, mockFile)).toStrictEqual(resultOutput);
+        expect(await service.readAndParseFile(inputData, filePath)).toStrictEqual(resultOutput);
     });
+
+    it('valid small input',async()=>{
+        const filePath = createNumberOfLineCSVFile(['school_id','grade','count'],100,'list_valid_small.csv');
+
+        const inputData = {
+            "ingestion_type": "event",
+            "ingestion_name": "student_attendance"
+        };
+        let resultOutput = {code: 200, message: 'CSV Uploaded Successfully'};
+        expect(await service.readAndParseFile(inputData, filePath)).toStrictEqual(resultOutput);
+    });
+
+    it('invalid less  csv format for api', async () => {
+        const filePath = createNumberOfLineCSVFile(['school_id','grade'],100,'list_error_small.csv');
+        await errorCSVTest(service,filePath)
+
+    });
+
+    it('invalid large  csv format for api', async () => {
+        const filePath = createNumberOfLineCSVFile(['school_id','grade'],1001,'list_error_large.csv');
+        await errorCSVTest(service,filePath)
+    });
+
 });
 
-const checkIfFileOrDirectoryExists = (path: string): boolean => {
-    return fs.existsSync(path);
-};
+async function errorCSVTest(service,filePath){
+    const errorResponse = {
+        response: {
+            data: {
+                "message": [
+                    {
+                        "instancePath": "/event/0",
+                        "schemaPath": "#/properties/event/items/required",
+                        "keyword": "required",
+                        "params": {
+                            "missingProperty": "date"
+                        },
+                        "message": "must have required property 'date'"
+                    }
+                ]
+            }
+        }
+    };
+    const mockHttpservice = {
+        post: jest.fn().mockImplementation(() => {
+            throw errorResponse;
+        })
+    };
+    const localModule: TestingModule = await Test.createTestingModule({
+        providers: [HttpCustomService, GenericFunction, CsvImportService,
+            {
+                provide: HttpCustomService,
+                useValue: mockHttpservice
+            },
+            {
+                provide: GenericFunction,
+                useClass: GenericFunction
+            },
+            {
+                provide: CsvImportService,
+                useClass: CsvImportService
+            }],
+    }).compile();
+    service = localModule.get<CsvImportService>(CsvImportService);
 
-const createFile = async (
+    const inputData = {
+        "ingestion_type": "event",
+        "ingestion_name": "student_attendance"
+    };
+    let resultOutput = {code: 400, error: errorResponse.response.data.message};
+    try {
+        // promise reject is an error to be catched
+        const outputResult = await service.readAndParseFile(inputData, filePath);
+    } catch (someErr) {
+        expect(someErr).toStrictEqual(resultOutput);
+    }
+}
+
+// dynamically create csv file on specified number of line
+function createNumberOfLineCSVFile(columns,csvNumberOfLine,fileName){
+    let csvFileData = columns.join(',')+'\n';
+    const columnLength = columns.length;
+    let individualLine = [];
+    for(let i=0;i<csvNumberOfLine;i++){
+        individualLine=[];
+        for(let j=1;j<=columnLength;j++){
+            individualLine.push((i+j).toString());
+        }
+        csvFileData+=`${individualLine.join(',')}\n`
+    }
+    const dirName = './files/';
+    createFile(dirName, fileName, csvFileData);
+    return dirName+fileName;
+}
+
+const createFile = (
     path: string,
     fileName: string,
     data: string,
-): Promise<void> => {
-    if (!checkIfFileOrDirectoryExists(path)) {
+): void => {
+    if (!fs.existsSync(path)) {
         fs.mkdirSync(path);
     }
-    const writeFile = promisify(fs.writeFile);
+    fs.writeFileSync(`${path}/${fileName}`, data, 'utf8');
 
-    return await writeFile(`${path}/${fileName}`, data, 'utf8');
 };
