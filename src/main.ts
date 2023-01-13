d783a19c74e7fb6fc247b1b008278b86d72ba375
dhanush-2397 <dhanush.ks@tibilsolutions.com>
2023-01-13 10:27:17 +0530

    added body parser

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import { urlencoded, json } from 'express';
-
+import * as bodyParser from 'body-parser';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.use(json({ limit: '50mb' }));
-    app.use(urlencoded({ extended: true, limit: '50mb' }));
+    // app.use(json({ limit: '50mb' }));
+    // app.use(urlencoded({ extended: true, limit: '50mb' }));
+    app.use(bodyParser.json({limit: '50mb'}));
+    app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
     await app.listen(3000);
     //running on port 3000
 }
